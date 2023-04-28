76fbc2bde51143f39995d571fb5ce1d276cbc16d
unknown <you@example.com>
2023-04-28 08:24:17 +0600

    form register

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,35 +1,86 @@
-import React, {useState} from 'react';
-import {Link} from "react-router-dom";
-import {BsPencilFill} from 'react-icons/bs'
+import React, {useContext, useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import {FaPencilAlt} from 'react-icons/fa'
+import {AiFillEye,AiFillEyeInvisible} from 'react-icons/ai'
+import axios from "axios";
+import {CustomContext} from "../../utils/Context";
 
 const Form = () => {
 
     const [status, setStatus] = useState(false)
+    const [email, setEmail] = useState('')
+
+    const navigate = useNavigate()
+
+    const [eye, setEye] = useState(false)
+
+    const {user, setUser} = useContext(CustomContext)
 
 
+    const registerUser = (e) => {
+        e.preventDefault()
+
+        let newUser = {
+            email,
+            password: e.target[0].value
+        }
+        axios.post('http://localhost:8080/register' , newUser)
+            .then(({data}) => {
+                setUser({
+                    token: data.accessToken,
+                    ...data.user
+                })
+
+                localStorage.setItem('user', JSON.stringify({
+                    token: data.accessToken,
+                    ...data.user
+                }))
+                navigate('/')
+            })
+            .catch((err) => console.log(err.message) )
+
+    }
 
     return (
-        <div className="content">
-            <form className='form'>
+        <div className='content'>
+            <form action="" className='form' onSubmit={registerUser}>
+
                 {
-                    status && <p className='form__email'>kubanovski.01@gmail.com <BsPencilFill/></p>
-
+                    status && <p className='form__email' onClick={() => setStatus(false)}>{email} <FaPencilAlt/> </p>
                 }
-               <h2 className='form__title'>Регистрация</h2>
 
-                <h2 className='form__title'> Придумай пароль для входа <br/>
-                     на любом устройстве</h2>
-                <input className='form__field' placeholder="Введите Email" type="text"/>
+                <h2 className='form__title'>
+                    {
+                        status ? 'Придумай пароль для входа на любом устройстве' : 'Регистрация'
+                    }
+                </h2>
 
-                <button className='form__btn' type="button">Продолжить</button>
+                {
+                    status && <>
+                        <div className='form__password'>
+                            <input className='form__field' placeholder='Придумайте пароль' type={eye ? 'text' : 'password'}/>
+                            <span className='form__eye' onClick={() => setEye(prev => !prev)}>
+                                {
+                                    eye ?  <AiFillEyeInvisible/> : <AiFillEye/>
+                                }
 
-                <input className='form__field' placeholder="Придумайте пароль"  type="text"/>
+                            </span>
+                        </div>
 
-                <button className='form__btn' type="submit">Создать аккаунт</button>
+                        <button className='form__btn' type='submit'>Создать аккаунт</button>
+                    </>
+                }
 
-                <Link to='/login'>У меня есть аккаунт</Link>
+                {
+                    !status &&
+                    <>
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} className='form__field' placeholder='Введите Email' type="text"/>
+                        <div onClick={() => setStatus(true)} className='form__btn'>Продолжить</div>
+                        <Link to='/login'>У меня есть аккаунт</Link>
+                    </>
+                }
+
             </form>
-
         </div>
     );
 };
--- src/utils/Context.js
+++ src/utils/Context.js
@@ -1,15 +1,26 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
+
 
 export const CustomContext = createContext()
 
 export const Context = (props) => {
 
 
-    const [count , setCount] = useState(0)
+    const [user, setUser] = useState({
+        email : ''
+    })
+
+    useEffect(() => {
+        if (localStorage.getItem('user') !== null){
+            setUser(JSON.parse(localStorage.getItem('user')))
+        }
+
+    }, [])
+
 
     const value = {
-        count,
-        setCount
+        user,
+        setUser
     }
 
     return <CustomContext.Provider value={value}>
